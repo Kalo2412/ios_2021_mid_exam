4331bf8505de57925dee7e11ba3585d7b357ebb3
Kaloyan <kaloyan.va@abv.bg>
2022-01-07 14:50:28 +0200

    Initial Commit

---
+++ ExamApp_07_Jan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExamApp_07_Jan
+//
+//  Created by Kaloyan Vachkov on 7.01.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
